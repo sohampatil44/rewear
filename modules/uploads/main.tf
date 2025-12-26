ef01fddcee1883e77300ed76f9fb479a2c57f7fa
Soham Patil <sohampatil@Sohams-MacBook-Air.local>
2025-12-26 20:47:16 +0530

    chore: wip infra changes [skip ci]

--- modules/uploads/main.tf
+++ modules/uploads/main.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "rewear_uploads" {
     bucket = "rewear-uploads-bucket"
-    acl = "public-read"
 
     versioning {
         enabled = true
@@ -10,6 +9,31 @@ resource "aws_s3_bucket" "rewear_uploads" {
         Name = "RewearUploads"
         Environment = "Production"
     }
+  
+}
+resource "aws_s3_bucket_public_access_block" "uploads_public_access" {
+    bucket = aws_s3_bucket.rewear_uploads.id
+
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
+  
+}
+resource "aws_s3_bucket_policy" "uploads_policy" {
+    bucket = aws_s3_bucket.rewear_uploads.id
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect    = "Allow"
+                Principal = "*"
+                Action    = "s3:GetObject"
+                Resource  = "${aws_s3_bucket.rewear_uploads.arn}/*"
+            }
+        ]
+    })
+    depends_on = [ aws_s3_bucket_public_access_block.uploads_public_access ]
   
 }
 
