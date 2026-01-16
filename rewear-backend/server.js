8078bc3c4574f9688d8129d2fd55e9d3eedbfbe1
Soham Patil <sohampatil@Sohams-MacBook-Air.local>
2026-01-16 20:18:59 +0530

    chore: update something [skip ci]

--- rewear-backend/server.js
+++ rewear-backend/server.js
@@ -75,4 +75,7 @@ mongoose
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+- app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
++ app.listen(PORT, "0.0.0.0", () =>
++   console.log(`🚀 Server running on port ${PORT}`)
++ );
