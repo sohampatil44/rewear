91ec2ce07185775141fd08e65341b7a3529969de
Soham Patil <sohampatil@Sohams-MacBook-Air.local>
2026-02-01 16:08:33 +0530

    latest updates [skip ci]

--- modules/firehose-logs/iam.tf
+++ modules/firehose-logs/iam.tf
@@ -17,12 +17,21 @@ resource "aws_iam_role_policy" "firehose_policy" {
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["s3:PutObject", "s3:ListBucket"]
-        Resource = ["${aws_s3_bucket.eks_logs.arn}/*"]
-      }
-    ]
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "s3:AbortMultipartUpload",
+        "s3:GetBucketLocation",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:PutObject"
+      ]
+      Resource = [
+        aws_s3_bucket.eks_logs.arn,
+        "${aws_s3_bucket.eks_logs.arn}/*"
+      ]
+    }]
   })
 }
+
--- modules/firehose-logs/main.tf
+++ modules/firehose-logs/main.tf
@@ -8,7 +8,7 @@ resource "aws_kinesis_firehose_delivery_stream" "eks_logs" {
     destination = "extended_s3"
 
     extended_s3_configuration {
-      role_arn = aws_iam_role.firehose.arn
+      role_arn = aws_iam_role.firehose_role.arn
       bucket_arn = aws_s3_bucket.eks_logs.arn
 
       prefix ="eks-logs/!{timestamp:yyyy/MM/dd}/"
