664fa6dea5e0e18cfefcf9f7169f38f638c5d4ee
Soham Patil <sohampatil@Sohams-MacBook-Air.local>
2026-03-07 17:06:17 +0530

    changes [skip ci]

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -159,6 +159,17 @@ resource "aws_eks_node_group" "ec2_workers" {
 }
 
 ################################
+# CLUSTER AUTOSCALER SERVICE ACCOUNT
+resource "'aws_security_group_rule'" "'allow_nodeport'"{
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+  security_group_id = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+################################
 # CLUSTER AUTOSCALER POLICY
 ################################
 resource "aws_iam_policy" "cluster_autoscaler" {
